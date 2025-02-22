eff6a40537573a9907fe61305444baf348268f02
Goshido <GoshidoMatazuki@gmail.com>
2025-02-22 22:16:08 +0300

    pbr: - Text alpha solver

--- app/src/main/hlsl/ui_common.ps.hlsl
+++ app/src/main/hlsl/ui_common.ps.hlsl
@@ -5,6 +5,16 @@
 #include "pbr/ui_program.inc"
 
 
+// P means Bezier control point
+static float16_t2 const             COLOR_ALPHA_P_X = float16_t2 ( 7.48e-1H, 3.99e-1H );
+static float16_t2 const             COLOR_ALPHA_P_Y = float16_t2 ( 8.04e-1H, 1.0H );
+
+static float16_t2 const             LUMA_BOTTOM_P1 = float16_t2 ( 5.4e-1H, 2.7e-1H );
+static float16_t2 const             LUMA_BOTTOM_P2 = float16_t2 ( 9.9e-1H, 6.18e-1H );
+
+static float16_t2 const             LUMA_TOP_P1 = float16_t2 ( 3.0e-2H, 2.12e-1H );
+static float16_t2 const             LUMA_TOP_P2 = float16_t2 ( 3.42e-1H, 7.18e-1H );
+
 [[vk::binding ( BIND_ATLAS_SAMPLER, SET_COMMON )]]
 SamplerState                        g_atlasSampler:     register ( s0 );
 
@@ -34,6 +44,61 @@ struct InputData
 
 //----------------------------------------------------------------------------------------------------------------------
 
+// FUCK - make a reference to documentation.
+
+float16_t SolveNewton ( in float16_t p1, in float16_t p2, in float16_t p, in float16_t guess )
+{
+    float16_t2 const vv = 3.0H * float16_t2 ( p1, p2 );
+    float16_t const a = vv.x - vv.y + 1.0H;
+    float16_t const b = mad ( -2.0H, vv.x, vv.y );
+    float16_t const xi = 3.0H * a;
+    float16_t const zeta = 2.0H * b;
+    p = -p;
+
+    [unroll ( 3 )]
+    for ( uint16_t i = 0U; i < 3U; ++i )
+    {
+        float16_t const gamma = guess * guess;
+        float16_t const t0 = mad ( a, guess, b );
+        float16_t const t1 = mad ( vv.x, guess, p );
+        float16_t const t2 = mad ( zeta, guess, vv.x );
+        guess = guess - mad ( gamma, t0, t1 ) / mad ( xi, gamma, t2 );
+    }
+
+    return guess;
+}
+
+float16_t CubicBezier ( in float16_t p1, in float16_t p2, in float16_t t )
+{
+    float16_t const t2 = t * t;
+    float16_t const w0 = t * p1;
+    float16_t const w1 = mad ( -2.0H, p1, p2 );
+    float16_t const w2 = ( p1 - p2 + 3.33333e-1H ) * t2;
+    float16_t const w3 = mad ( w1, t2, w0 );
+    return 3.0H * mad ( w2, t, w3 );
+}
+
+float16_t ResolveTextAlpha ( float16_t colorAlpha, float16_t glyphLuma )
+{
+    // FUCK simplify to branchless
+    float16_t guess;
+
+    if ( colorAlpha < 3.72545e-1H )
+        guess = 5.8e-1H * colorAlpha;
+    else if ( colorAlpha < 3.72545e-1H )
+        guess = mad ( 1.6H, colorAlpha, -3.8e-1H );
+    else
+        guess = mad ( 7.0e-1H, colorAlpha, 3.0e-1H );
+
+    float16_t const t = SolveNewton ( COLOR_ALPHA_P_X.x, COLOR_ALPHA_P_X.y, colorAlpha, guess );
+    float16_t const lumaLerp = CubicBezier ( COLOR_ALPHA_P_Y.x, COLOR_ALPHA_P_Y.y, t );
+
+    float16_t2 const p1 = lerp ( LUMA_BOTTOM_P1, LUMA_TOP_P1, lumaLerp );
+    float16_t2 const p2 = lerp ( LUMA_BOTTOM_P2, LUMA_TOP_P2, lumaLerp );
+
+    return CubicBezier ( p1.y, p2.y, SolveNewton ( p1.x, p2.x, glyphLuma, glyphLuma ) );
+}
+
 float16_t4 Compute ( in InputData inputData )
 {
     float16_t4 const color = (float16_t4)inputData._color;
@@ -42,10 +107,11 @@ float16_t4 Compute ( in InputData inputData )
         color * (float16_t4)g_imageTexture.SampleLevel ( g_imageSampler, inputData._imageUV, 0.0F );
 
     float32_t3 const atlasCoordinate = float32_t3 ( inputData._atlasUV, inputData._atlasLayer );
-    float16_t const atlasSample = (float16_t)g_atlasTexture.SampleLevel ( g_atlasSampler, atlasCoordinate, 0.0F ).x;
-    float16_t4 const textPart = float16_t4 ( color.xyz, atlasSample * color.a );
+    float16_t const glyphLuma = (float16_t)g_atlasTexture.SampleLevel ( g_atlasSampler, atlasCoordinate, 0.0F ).x;
+    float16_t const textAlpha = ResolveTextAlpha ( color.a, glyphLuma );
+    float16_t4 const textPart = float16_t4 ( color.xyz, textAlpha );
 
-    return lerp ( textPart, imagePart, (float16_t4)( atlasSample == 0.0F ) );
+    return lerp ( textPart, imagePart, textAlpha == 0.0F );
 }
 
 

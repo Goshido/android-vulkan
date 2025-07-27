409a4fc72e30fb0f1cfcd6d6d17f179effdc7e5a
Goshido <GoshidoMatazuki@gmail.com>
2025-07-27 14:01:23 +0300

    core: - added checking fullDrawIndexUint32 on PC

--- app/src/main/cpp/sources/platform/windows/renderer.cpp
+++ app/src/main/cpp/sources/platform/windows/renderer.cpp
@@ -389,6 +389,9 @@ bool Renderer::CheckRequiredFeatures ( std::vector<std::string> const &deviceExt
 
 #endif // AV_ENABLE_NSIGHT
 
+        // FUCK - remove after vertex pulling
+        AV_BITWISE ( CheckFeature ( features.fullDrawIndexUint32, "fullDrawIndexUint32" ) ) &
+
         AV_BITWISE ( CheckFeature ( features.samplerAnisotropy, "samplerAnisotropy" ) ) &
         AV_BITWISE ( CheckFeature ( features.shaderInt16, "shaderInt16" ) ) &
         AV_BITWISE ( CheckFeature ( features.shaderInt64, "shaderInt64" ) ) &
@@ -638,7 +641,10 @@ VkPhysicalDeviceFeatures2 Renderer::GetRequiredPhysicalDeviceFeatures () noexcep
         .features
         {
             .robustBufferAccess = VK_FALSE,
+
+            // FUCK - remove after vertex pulling
             .fullDrawIndexUint32 = VK_TRUE,
+
             .imageCubeArray = VK_FALSE,
             .independentBlend = VK_FALSE,
             .geometryShader = VK_FALSE,
